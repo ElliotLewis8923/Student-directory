ae39b8c535e9f35e0bd7b7e8bb59df5a7ba9675f
elliotlewis8923 <elliotlewis8923@gmail.com>
2014-08-06 16:10:46 +0100

    added more methods

--- Student-directory/directory.rb
+++ Student-directory/directory.rb
@@ -1,29 +1,29 @@
-students = [
-{:name => "Javier Silverio", :cohort => :August},
-{:name => "Elliot Lewis", :cohort => :August},
-{:name => "Ben Tillett", :cohort => :August},
-{:name => "Vincent Koch", :cohort => :August},
-{:name => "Michelle Ballard", :cohort => :August},
-{:name => "Nick Roberts", :cohort => :August},
-{:name => "Tatiana Soukiassian", :cohort => :August},
-{:name => "Mervé Silk", :cohort => :August},
-{:name => "Albert Vallerdu", :cohort => :August},
-{:name => "Lovis Schultze", :cohort => :August},
-{:name => "Chris Oatley", :cohort => :August},
-{:name => "Spike Lindsey", :cohort => :August},
-{:name => "Henry Stanley", :cohort => :August},
-{:name => "Ruth Earle", :cohort => :August},
-{:name => "Andy Gates", :cohort => :August},
-{:name => "Marc Singh", :cohort => :August},
-{:name => "Faisal Aydarus", :cohort => :August},
-{:name => "Ethel Ng", :cohort => :August},
-{:name => "Kevin Daniells", :cohort => :August},
-{:name => "Maya Driver", :cohort => :August},
-{:name => "Leopold Kwok", :cohort => :August},
-{:name => "James McNeil", :cohort => :August},
-{:name => "Jerome Pratt", :cohort => :August},
-{:name => "David Wickes", :cohort => :August}
-]
+# students = [
+# {:name => "Javier Silverio", :cohort => :August},
+# {:name => "Elliot Lewis", :cohort => :August},
+# {:name => "Ben Tillett", :cohort => :August},
+# {:name => "Vincent Koch", :cohort => :August},
+# {:name => "Michelle Ballard", :cohort => :August},
+# {:name => "Nick Roberts", :cohort => :August},
+# {:name => "Tatiana Soukiassian", :cohort => :August},
+# {:name => "Mervé Silk", :cohort => :August},
+# {:name => "Albert Vallerdu", :cohort => :August},
+# {:name => "Lovis Schultze", :cohort => :August},
+# {:name => "Chris Oatley", :cohort => :August},
+# {:name => "Spike Lindsey", :cohort => :August},
+# {:name => "Henry Stanley", :cohort => :August},
+# {:name => "Ruth Earle", :cohort => :August},
+# {:name => "Andy Gates", :cohort => :August},
+# {:name => "Marc Singh", :cohort => :August},
+# {:name => "Faisal Aydarus", :cohort => :August},
+# {:name => "Ethel Ng", :cohort => :August},
+# {:name => "Kevin Daniells", :cohort => :August},
+# {:name => "Maya Driver", :cohort => :August},
+# {:name => "Leopold Kwok", :cohort => :August},
+# {:name => "James McNeil", :cohort => :August},
+# {:name => "Jerome Pratt", :cohort => :August},
+# {:name => "David Wickes", :cohort => :August}
+# ]
 def print_header
 puts "The students of my cohort at Makers Academy"
 puts "------------------"
@@ -36,6 +36,20 @@ end
 def print_footer(names)
 	puts "Overall, we have #{names.length} great students"
 end
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	students = []
+	name = gets.chomp
+	while !name.empty? do
+		students << {:name => name, :cohort => :November}
+		puts "Now we have #{students.length} students"
+		name = gets.chomp
+	end
+students
+end
+students = input_students
 print_header
 print(students)
 print_footer(students)
+
