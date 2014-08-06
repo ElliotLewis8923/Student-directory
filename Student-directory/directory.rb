dc28aea5a0c08e9329c79d9dca2ba18cb348b7b4
elliotlewis8923 <elliotlewis8923@gmail.com>
2014-08-06 12:25:55 +0100

    added arrays

--- Student-directory/directory.rb
+++ Student-directory/directory.rb
@@ -1,29 +1,57 @@
+
+students = [
+"Javier Silverio",
+"Elliot Lewis",
+"Ben Tillett",
+"Vincent Koch",
+"Michelle Ballard",
+"Nick Roberts",
+"Tatiana Soukiassian",
+"Mervé Silk",
+"Albert Vallerdu",
+"Lovis Schultze",
+"Chris Oatley",
+"Spike Lindsey",
+"Henry Stanley",
+"Ruth Earle",
+"Andy Gates",
+"Marc Singh",
+"Faisal Aydarus",
+"Ethel Ng",
+"Kevin Daniells",
+"Maya Driver",
+"Leopold Kwok",
+"James McNeil",
+"Jerome Pratt",
+"David Wickes"
+]
 puts "The students of my cohort at Makers Academy"
 puts "------------------"
-puts "Javier Silverio"
-puts "Elliot Lewis"
-puts "Ben Tillett"
-puts "Vincent Koch"
-puts "Michelle Ballard"
-puts "Nick Roberts"
-puts "Tatiana Soukiassian"
-puts "Mervé Silk"
-puts "Albert Vallerdu"
-puts "Lovis Schultze"
-puts "Chris Oatley"
-puts "Spike Lindsey"
-puts "Henry Stanley"
-puts "Ruth Earle"
-puts "Andy Gates"
-puts "Marc Singh"
-puts "Faisal Aydarus"
-puts "Ethel Ng"
-puts "Kevin Daniells"
-puts "Maya Driver"
-puts "Leopold Kwok"
-puts "James McNeil"
-puts "Jerome Pratt"
-puts "David Wickes"
-student_count = 24
-puts "Overall, we have #{student_count} great students"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
+puts students[19]
+puts students[20]
+puts students[21]
+puts students[22]
+puts students[23]
+puts students[24]
+puts "Overall, we have #{students.length} great students"
+
 
