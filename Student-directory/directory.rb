32c57dd99196b6a45bba71cb43d99ae5d8feee5c
elliotlewis8923 <elliotlewis8923@gmail.com>
2014-08-06 13:38:47 +0100

    added hashes

--- Student-directory/directory.rb
+++ Student-directory/directory.rb
@@ -1,36 +1,36 @@
 students = [
-"Javier Silverio",
-"Elliot Lewis",
-"Ben Tillett",
-"Vincent Koch",
-"Michelle Ballard",
-"Nick Roberts",
-"Tatiana Soukiassian",
-"Mervé Silk",
-"Albert Vallerdu",
-"Lovis Schultze",
-"Chris Oatley",
-"Spike Lindsey",
-"Henry Stanley",
-"Ruth Earle",
-"Andy Gates",
-"Marc Singh",
-"Faisal Aydarus",
-"Ethel Ng",
-"Kevin Daniells",
-"Maya Driver",
-"Leopold Kwok",
-"James McNeil",
-"Jerome Pratt",
-"David Wickes"
+{:name => "Javier Silverio", :cohort => :August},
+{:name => "Elliot Lewis", :cohort => :August},
+{:name => "Ben Tillett", :cohort => :August},
+{:name => "Vincent Koch", :cohort => :August},
+{:name => "Michelle Ballard", :cohort => :August},
+{:name => "Nick Roberts", :cohort => :August},
+{:name => "Tatiana Soukiassian", :cohort => :August},
+{:name => "Mervé Silk", :cohort => :August},
+{:name => "Albert Vallerdu", :cohort => :August},
+{:name => "Lovis Schultze", :cohort => :August},
+{:name => "Chris Oatley", :cohort => :August},
+{:name => "Spike Lindsey", :cohort => :August},
+{:name => "Henry Stanley", :cohort => :August},
+{:name => "Ruth Earle", :cohort => :August},
+{:name => "Andy Gates", :cohort => :August},
+{:name => "Marc Singh", :cohort => :August},
+{:name => "Faisal Aydarus", :cohort => :August},
+{:name => "Ethel Ng", :cohort => :August},
+{:name => "Kevin Daniells", :cohort => :August},
+{:name => "Maya Driver", :cohort => :August},
+{:name => "Leopold Kwok", :cohort => :August},
+{:name => "James McNeil", :cohort => :August},
+{:name => "Jerome Pratt", :cohort => :August},
+{:name => "David Wickes", :cohort => :August}
 ]
 def print_header
 puts "The students of my cohort at Makers Academy"
 puts "------------------"
 end
-def print(names)
-names.each do |name|
-	puts name
+def print(students)
+names.each do |student|
+	puts "#{student[:name]}, (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(names)
