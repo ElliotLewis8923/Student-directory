d528e8032fadfa2c5dc1b2eb6804eebbbc5b5473
elliotlewis8923 <elliotlewis8923@gmail.com>
2014-08-06 21:14:53 +0100

    completed v.6 ex. 3

--- Student-directory/directory.rb
+++ Student-directory/directory.rb
@@ -24,17 +24,25 @@
 # {:name => "Jerome Pratt", :cohort => :August},
 # {:name => "David Wickes", :cohort => :August}
 # ]
+
 def print_header
 print "The students of my cohort at Makers Academy\n------------------\n"
 end
-def put(students)
-students.each_with_index do |student, index|
-	print "#{index+1}. #{student[:name]}, (#{student[:cohort]} cohort)\n"
-  end
-end
+
 def print_footer(names)
-	print "Overall, we have #{names.length} great students\n"
+	print "Overall, we have #{names.length} great students\n" 
 end
+
+def first_letter(student)
+	student.select {|first| first[:name][0] == "A"}
+	end
+
+def put(student, first)
+	first.each do |student, first|
+		print "#{student[:name]} (#{student[:cohort]} cohort)\n"
+	end
+end
+
 def input_students
 	print "Please enter the names of the students\nTo finish, just hit return twice\n"
 	students = []
@@ -42,12 +50,16 @@ def input_students
 	while !name.empty? do
 		students << {:name => name, :cohort => :November}
 		print "Now we have #{students.length} students\n"
+
 		name = gets.chomp
 	end
-students
+	students
 end
+
 students = input_students
 print_header
-put(students)
+first_letter(students)
+put(students, first_letter(students))
 print_footer(students)
+
 
