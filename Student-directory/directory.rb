07cc60c8c294482f75002065966f01e7e7bcec7a
elliotlewis8923 <elliotlewis8923@gmail.com>
2014-08-06 21:28:41 +0100

    completed v.6 ex.4

--- Student-directory/directory.rb
+++ Student-directory/directory.rb
@@ -1,30 +1,3 @@
-# students = [
-# {:name => "Javier Silverio", :cohort => :August},
-# {:name => "Elliot Lewis", :cohort => :August},
-# {:name => "Ben Tillett", :cohort => :August},
-# {:name => "Vincent Koch", :cohort => :August},
-# {:name => "Michelle Ballard", :cohort => :August},
-# {:name => "Nick Roberts", :cohort => :August},
-# {:name => "Tatiana Soukiassian", :cohort => :August},
-# {:name => "Mervé Silk", :cohort => :August},
-# {:name => "Albert Vallerdu", :cohort => :August},
-# {:name => "Lovis Schultze", :cohort => :August},
-# {:name => "Chris Oatley", :cohort => :August},
-# {:name => "Spike Lindsey", :cohort => :August},
-# {:name => "Henry Stanley", :cohort => :August},
-# {:name => "Ruth Earle", :cohort => :August},
-# {:name => "Andy Gates", :cohort => :August},
-# {:name => "Marc Singh", :cohort => :August},
-# {:name => "Faisal Aydarus", :cohort => :August},
-# {:name => "Ethel Ng", :cohort => :August},
-# {:name => "Kevin Daniells", :cohort => :August},
-# {:name => "Maya Driver", :cohort => :August},
-# {:name => "Leopold Kwok", :cohort => :August},
-# {:name => "James McNeil", :cohort => :August},
-# {:name => "Jerome Pratt", :cohort => :August},
-# {:name => "David Wickes", :cohort => :August}
-# ]
-
 def print_header
 print "The students of my cohort at Makers Academy\n------------------\n"
 end
@@ -34,7 +7,7 @@ def print_footer(names)
 end
 
 def first_letter(student)
-	student.select {|first| first[:name][0] == "A"}
+	student.select {|first| first[:name].length < 12}
 	end
 
 def put(student, first)
@@ -61,5 +34,3 @@ print_header
 first_letter(students)
 put(students, first_letter(students))
 print_footer(students)
-
-
